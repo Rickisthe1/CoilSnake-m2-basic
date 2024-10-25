f76fbf6a4989124855796a04fe77ea26d0c621cf
Gabbi (Rickisthe1) <33672357+Rickisthe1@users.noreply.github.com>
2024-10-25 14:04:07 -0400

    Naming Skip

--- coilsnake/modules/eb/SkipNamingModule.py
+++ coilsnake/modules/eb/SkipNamingModule.py
@@ -42,8 +42,9 @@ class SkipNamingModule(EbModule):
             # it works by changing the jump from @CHANGE_TO_NAMING_SCREEN_MUSIC to @UNKNOWN18 (which normally runs directly after the music change)
             # https://github.com/Herringway/ebsrc/blob/87f514cb4b77fa3193bcb122ea51f5de5cfdd9cf/src/intro/file_select_menu_loop.asm#L101
             # Verify the code structure first:
-            if rom[0x1F754] == 0xD0 and rom[0x1F755] == 0x09: #$c/1f8f0 - $c/1f8f1
-                rom[0x1F755] = 0x10 #$c/1f8f1
+            if rom[0x1F752] == 0xF0 and rom[0x1F753] == 0xE5: #$c/1f8f0 - $c/1f8f1
+                rom[0x1F754] = 0x80
+                rom[0x1F755] = 0x05
             else:
                 log.warn("Unable to apply naming screen music bypass due to existing ASM changes")
 
@@ -56,15 +57,15 @@ class SkipNamingModule(EbModule):
             rom[offset:offset+4] = [0x48, 0x08, 0xe2, 0x20]
             offset += 4
 
-            offset = self.write_loader_asm(rom, offset, self.data["Name1"], 5, 0xce, 0x99)
-            offset = self.write_loader_asm(rom, offset, self.data["Name2"], 5, 0x2d, 0x9a)
-            offset = self.write_loader_asm(rom, offset, self.data["Name3"], 5, 0x8c, 0x9a)
-            offset = self.write_loader_asm(rom, offset, self.data["Name4"], 5, 0xeb, 0x9a)
-            offset = self.write_loader_asm(rom, offset, self.data["Pet"],   6, 0x19, 0x98)
-            offset = self.write_loader_asm(rom, offset, self.data["Food"],  6, 0x1f, 0x98)
-            offset = self.write_loader_asm(rom, offset, self.data["Thing"], 6, 0x29, 0x98)
+            offset = self.write_loader_asm(rom, offset, self.data["Name1"], 4, 0x7f, 0x9c) #0xce, 0x99
+            offset = self.write_loader_asm(rom, offset, self.data["Name2"], 4, 0xdd, 0x9c) #0x2d, 0x9a
+            offset = self.write_loader_asm(rom, offset, self.data["Name3"], 4, 0x3b, 0x9d) #0x8c, 0x9a
+            offset = self.write_loader_asm(rom, offset, self.data["Name4"], 4, 0x99, 0x9d) #0xeb, 0x9a
+            offset = self.write_loader_asm(rom, offset, self.data["Pet"],   6, 0xcd, 0x9a) #0x19, 0x98
+            offset = self.write_loader_asm(rom, offset, self.data["Food"],  6, 0xd3, 0x9a) #0x1f, 0x98
+            offset = self.write_loader_asm(rom, offset, self.data["Thing"], 6, 0xdb, 0x9a) #0x29, 0x98
 
             if self.data["Enable Summary"]:
-                rom[offset:offset+6] = [0x28, 0x68, 0x5c, 0xc0, 0xfa, 0xc1]
+                rom[offset:offset+6] = [0x28, 0x68, 0x5c, 0x0c, 0xf9, 0xc1] #0xc0, 0xfa, 0xc1
             else:
-                rom[offset:offset+6] = [0x28, 0x68, 0x5c, 0x05, 0xfd, 0xc1]
+                rom[offset:offset+6] = [0x28, 0x68, 0x5c, 0xb3, 0xfa, 0xc1] #0x05, 0xfd, 0xc1
