c3a749db189b7c5f522757fd4608e9300b883fc3
Cooper Harasyn <charasyn@outlook.com>
2025-02-26 21:32:54 -0800

    Refactor translation manager into gui and non-gui parts

--- coilsnake/ui/common.py
+++ coilsnake/ui/common.py
@@ -6,6 +6,7 @@ from shutil import copyfile
 import time
 import sys
 from ccscript import ccc
+from collections import defaultdict
 
 from CCScriptWriter.CCScriptWriter import CCScriptWriter
 
@@ -23,6 +24,46 @@ from coilsnake.util.common.assets import open_asset, ccscript_library_path
 
 log = logging.getLogger(__name__)
 
+# Path to language files
+LANGUAGE_FILES = {
+    "English": "coilsnake/lang/en.json",
+    "Japanese": "coilsnake/lang/jp.json",
+}
+
+class TranslationStringManager:
+    _TRANSLATIONS_LANGUAGE_NOT_LOADED = defaultdict( lambda: "Translation not loaded" )
+
+    @staticmethod
+    def _json_to_translations(json_data):
+        missing = "Missing localization string"
+        return defaultdict(lambda: missing, json_data)
+
+    @classmethod
+    def _load_language(cls, language):
+        file_path = LANGUAGE_FILES.get(language, None)
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                json_data = json.load(file)
+            return cls._json_to_translations(json_data)
+        except:
+            return None
+
+    def __init__(self):
+        self.translations = self._TRANSLATIONS_LANGUAGE_NOT_LOADED
+
+    def get(self, string_name: str) -> str:
+        return self.translations[string_name]
+
+    def change_language(self, language: str) -> None:
+        translations = self._load_language(language)
+        if not translations:
+            log.error("Unable to load translation file for language '%s'", language)
+            return
+        self.translations = translations
+        self._update_translated_strings()
+
+    def _update_translated_strings(self) -> None:
+        pass
 
 def setup_logging(quiet=False, verbose=False, stream=None):
     # Disable the weird "STREAM" logging messages in Pillow 3.0.0
